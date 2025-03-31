297b75ce6ccb4f2d6421ee3f7ef8a65cb1f65f30
noname <nomail@nomail.com>
2025-04-01 00:16:48 +0530

    Added callback for after login redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,6 +6,9 @@ export default function Login() {
   const signInWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`
+      }
     });
     if (error) console.error("Error logging in:", error.message);
   };
