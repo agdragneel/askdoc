5491be5723d9d653f342e03d239cc76703201c50
noname <nomail@nomail.com>
2025-04-04 00:10:27 +0530

    Completed and Passed Build Testing

--- lib/pdf-utils.ts
+++ lib/pdf-utils.ts
@@ -1,3 +1,5 @@
+import mammoth from "mammoth";
+
 export const extractTextFromPDF = async (file: File): Promise<string> => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
